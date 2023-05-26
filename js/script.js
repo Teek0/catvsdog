681cf2d3e62cd19ede2ead390c2e232cd68a4684
Nicole Sáez <86169717+Teek0@users.noreply.github.com>
2023-05-25 23:35:36 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ var animalImg=document.querySelector('#fav-animal');
 console.log(animalImg.src);
 
 function pickCat(){
-    animalImg.src="gato.jpeg";
+    animalImg.src="img/gato.jpeg";
 }
 function pickDog(){
-    animalImg.src="perro.jpeg";
+    animalImg.src="img/perro.jpeg";
 }
